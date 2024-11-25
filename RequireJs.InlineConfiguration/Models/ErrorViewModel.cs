4d49c0ec03dde416d447c34abac2a506f26f1041
Peter Marinov <peter@endatix.com>
2024-11-25 12:54:25 +0200

    feat: Updating SurveyJS with RequireJS sample with .NET 9, Razor Pages and latest SurveyJS set of dependencies (#3)

--- RequireJs.InlineConfiguration/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RequireJs.InlineConfiguration.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
